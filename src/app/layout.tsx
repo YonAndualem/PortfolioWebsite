18402253f3d60cfa7684e3c876d75e9a61b5c039
Yonas B.Andualem <yonasandualem1472@gmail.com>
2025-08-05 15:00:49 +0300

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,18 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://your-portfolio-domain.com'), // <-- Change this to your deployed domain!
+  metadataBase: new URL('https://yonandualem.netlify.app/'), // <-- Change this to your deployed domain!
   title: 'Yonas Berhanu Andualem | Full Stack Developer & UI/UX Designer',
   description: 'Portfolio of Yonas Berhanu Andualem – Software Engineering student at HiLCoE, ALX alumni, and Full Stack Developer. MERN Stack Specialist (React, Node.js, Express, MySQL) passionate about building user-centered applications with engaging interfaces.',
   generator: 'Next.js',
   openGraph: {
     title: 'Yonas Berhanu Andualem | Full Stack Developer & UI/UX Designer',
     description: 'A Software Engineering student at HiLCoE and ALX alumni, specializing in MERN stack (React, Node.js, Express, MySQL) and UI/UX design. Explore my portfolio, skills, and projects.',
-    url: 'https://your-portfolio-domain.com', // <-- Replace with your deployed domain
-    siteName: 'Yonas Berhanu Andualem Portfolio',
+    url: 'https://yonandualem.netlify.app/', // <-- Replace with your deployed domain
+    siteName: 'Yonas Berhanu Andualem\'s Portfolio',
     images: [
       {
-        url: '/opengraph-image.webp', // Place the image in /public
+        url: '/opengraph-image.png', // Place the image in /public
         width: 1200,
         height: 630,
         alt: 'Yonas Berhanu Andualem Portfolio',
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Yonas Berhanu Andualem | Full Stack Developer & UI/UX Designer',
     description: 'A Software Engineering student at HiLCoE and ALX alumni, specializing in MERN stack (React, Node.js, Express, MySQL) and UI/UX design.',
-    images: ['/opengraph-image.webp'],
+    images: ['/opengraph-image.png'],
   },
 }
 
